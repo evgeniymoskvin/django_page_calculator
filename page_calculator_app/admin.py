1a45925c6ab72381429f19abd56be09fa67694ba
MoskvinEI <E.I.Moskvin@el-spb.local>
2024-06-24 09:03:26 +0300

    upd work_status

--- page_calculator_app/admin.py
+++ page_calculator_app/admin.py
@@ -3,13 +3,13 @@ from page_calculator_app.models import EmployeeModel, MoreDetailsEmployeeModel, 
 
 class EmployeeAdmin(admin.ModelAdmin):
     # list_display = ("author", "text_task")
-    ordering = ["last_name", "first_name", "middle_name"]
+    ordering = ["-work_status", "last_name", "first_name", "middle_name"]
     search_fields = ["last_name", "first_name", "middle_name", "personnel_number", "user_phone"]
     list_filter = ("department_group__city_dep__city", "department_group__group_dep_abr", "department__command_number")
 
 
 class MoreDetailsEmployeeAdmin(admin.ModelAdmin):
-    ordering = ["emp__last_name", "emp__first_name", "emp__middle_name"]
+    ordering = ["emp__last_name", "emp__first_name", "emp__middle_name", ]
     search_fields = ["emp__last_name", "emp__first_name", "emp__middle_name"]
     list_filter = (
         "emp__department_group__city_dep__city", "emp__department_group__group_dep_abr",
